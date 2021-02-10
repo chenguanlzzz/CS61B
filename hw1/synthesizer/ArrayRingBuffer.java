b9d39874b4425ebdffc536160243588eef8d5cde
chenguanlzzz <52288848+chenguanlzzz@users.noreply.github.com>
2021-02-10 15:24:18 +0800

    Add files via upload
    Finish HW1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,100 @@
+// TODO: Make sure to make this class a part of the synthesizer package
+package synthesizer;
+
+import java.util.Iterator;
+
+//TODO: Make sure to make this class and all of its methods public
+//TODO: Make sure to make this class extend AbstractBoundedQueue<t>
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // TODO: Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+        //       this.capacity should be set appropriately. Note that the local variable
+        //       here shadows the field we inherit from AbstractBoundedQueue, so
+        //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     * @return
+     */
+    @Override
+    public void enqueue(T x) {
+        // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount += 1;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update first.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T removeItem = rb[first];
+        fillCount -= 1;
+        first = (first + 1) % capacity;
+        return removeItem;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Empty Buffer!");
+        }
+        return rb[first];
+    }
+
+    // TODO: When you get to part 5, implement the needed code to support iteration.
+    public Iterator<T> iterator() {
+        return new bufferIterator();
+    }
+    private class bufferIterator implements Iterator<T> {
+        private int wizPos = first;
+        private int count = 0;
+
+        @Override
+        public boolean hasNext() {
+            return count < fillCount;
+        }
+
+        @Override
+        public T next() {
+            T next = rb[wizPos];
+            wizPos += 1;
+            count += 1;
+            return next;
+        }
+    }
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,15 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(2);
+        assertTrue(arb.isEmpty());
+        arb.enqueue(1);
+        assertEquals((Integer) 1, arb.peek());
+        assertFalse(arb.isFull());
+        arb.enqueue(2);
+        assertTrue(arb.isFull());
+        assertEquals((Integer)1, arb.dequeue());
+        assertFalse(arb.isFull());
     }
 
     /** Calls tests for ArrayRingBuffer. */
