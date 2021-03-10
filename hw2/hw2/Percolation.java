dcec78742e3c4ac1234e1c910297f4a9e441fe4b
chenguanlzzz <52288848+chenguanlzzz@users.noreply.github.com>
2021-03-11 00:21:51 +0800

    Add files via upload
    Finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,115 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    boolean[][] percoMatrix;
+    int n;
+    WeightedQuickUnionUF percoUF;
+    int openSize;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must > 0");
+        }
+        n = N;
+        openSize = 0;
+        // true for open, false for blocked;
+        percoMatrix = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                percoMatrix[i][j] = false;
+            }
+        }
+
+        // the second to last is the secret node for top, last node is secret node for bottom;
+        percoUF = new WeightedQuickUnionUF(N * N + 2);
+        int top = N * N;
+        //int bot = N * N + 1;
+        for (int i = 0; i < N; i++) {
+            percoUF.union(i, top);
+            //percoUF.union(top - 1 - i, bot);
+        }
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row > n || col > n) {
+            throw new IndexOutOfBoundsException("Out of index.");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        percoMatrix[row][col] = true;
+        openSize += 1;
+        // Check the surrounding is fall or not, if it is, make this element fall
+        if (row > 0 && isOpen(row - 1, col)) {
+            percoUF.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+        }
+        if (row < n - 1 && isOpen(row + 1, col)) {
+            percoUF.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+        }
+        if (col > 0 && isOpen(row, col - 1)) {
+            percoUF.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+        }
+        if (col < n - 1 && isOpen(row , col + 1)) {
+            percoUF.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+        }
+        if (row == n - 1 && isFull(row, col)) {
+            percoUF.union(n * n + 1, xyTo1D(row, col));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row > n || col > n) {
+            throw new IndexOutOfBoundsException("Out of index.");
+        }
+        return percoMatrix[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row > n || col > n) {
+            throw new IndexOutOfBoundsException("Out of index.");
+        }
+        return percoUF.connected(xyTo1D(row, col), n * n);
+    }
+
+    // change the xy correspond to 1D number
+    private int xyTo1D (int x, int y) {
+        return x * n + y;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSize;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return percoUF.connected(n * n, n * n + 1);
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+        Percolation x = new Percolation(3);
+        x.open(0,0);
+        //System.out.println(x.isOpen(0, 0));
+        //System.out.println(x.isFull(0, 0));
+        //System.out.println(x.isFull(1, 0));
+        //System.out.println(x.isOpen(1, 0));
+        x.open(1, 0);
+        //System.out.println(x.isOpen(1, 0));
+        //System.out.println(x.isFull(1, 0));
+
+        x.open(2, 0);
+        System.out.println(x.percolates());
+        x.open(2,0);
+        x.open(2,2);
+        System.out.println(x.isFull(2,2));
+        System.out.println(x.numberOfOpenSites());
+
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,65 @@
 package hw2;
+import java.lang.*;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    int[] numArr;
+    double mean;
+    double stddev;
+    double confidenceLow;
+    double confidenHigh;
+    int N;
+    int T;
 
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be larger than 0");
+        }
+
+        // Array of the number of each percolation
+        numArr = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            Percolation percoSim = pf.make(N);
+            while (!percoSim.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                percoSim.open(row, col);
+            }
+            numArr[i] = percoSim.numberOfOpenSites();
+        }
+        mean = mean();
+        stddev = stddev();
+        confidenceLow = confidenceLow();
+        confidenHigh = confidenceHigh();
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(numArr);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(numArr);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public static void main (String[] args) {
+        PercolationStats x = new PercolationStats(800, 30, new PercolationFactory());
+        System.out.println(x.mean());
+    }
 }
