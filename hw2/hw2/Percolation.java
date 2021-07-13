b695446db45762588b95eff6c2971e4f36720eb4
chenguanlzzz <52288848+chenguanlzzz@users.noreply.github.com>
2021-07-13 17:56:34 +0800

    Add files via upload

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,15 +2,11 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Percolation {
     boolean[][] percoMatrix;
     int n;
     WeightedQuickUnionUF percoUF;
     int openSize;
-    Set<Integer> openTop, openBot;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -32,15 +28,14 @@ public class Percolation {
         //int top = N * N;
         //int bot = N * N + 1;
         //for (int i = 0; i < N; i++) {
-        //  percoUF.union(i, top);
+        //   percoUF.union(i, top);
         //percoUF.union(top - 1 - i, bot);
         //}
-        openTop = new HashSet<>();
-        openBot = new HashSet<>();
     }
 
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
+        int cur = xyTo1D(row, col);
         if (row > n || col > n) {
             throw new IndexOutOfBoundsException("Out of index.");
         }
@@ -49,7 +44,6 @@ public class Percolation {
         }
         percoMatrix[row][col] = true;
         openSize += 1;
-        int cur = xyTo1D(row, col);
         // Check the surrounding is fall or not, if it is, make this element fall
         if (row > 0 && isOpen(row - 1, col)) {
             percoUF.union(cur, xyTo1D(row - 1, col));
@@ -63,49 +57,11 @@ public class Percolation {
         if (col < n - 1 && isOpen(row, col + 1)) {
             percoUF.union(cur, xyTo1D(row, col + 1));
         }
-        if (row == n - 1) {
-            if (!percolates()) {
-                percoUF.union(n * n + 1, cur);
-            } else {
-                if (percoUF.connected(cur, n * n)) {
-                    percoUF.union(n * n + 1, cur);
-                } else {
-                    boolean notPer = true;
-                    for (int top : openTop) {
-                        if (percoUF.connected(cur, top)) {
-                            percoUF.union(n * n + 1, cur);
-                            openTop.remove(top);
-                            notPer = false;
-                            break;
-                        }
-                    }
-                    if (notPer) {
-                        openBot.add(cur);
-                    }
-                }
-            }
+        if (row == n - 1 && isFull(row, col)) {
+            percoUF.union(n * n + 1, cur);
         }
         if (row == 0) {
-            if (!percolates()) {
-                percoUF.union(n * n, cur);
-            } else {
-                if (percoUF.connected(cur, n * n + 1)) {
-                    percoUF.union(n * n, cur);
-                } else {
-                    boolean notPer = true;
-                    for (int bot : openBot) {
-                        if (percoUF.connected(cur, bot)) {
-                            percoUF.union(n * n + 1, cur);
-                            openBot.remove(bot);
-                            notPer = false;
-                            break;
-                        }
-                    }
-                    if (notPer) {
-                        openTop.add(cur);
-                    }
-                }
-            }
+            percoUF.union(n * n, cur);
         }
     }
 
@@ -122,9 +78,7 @@ public class Percolation {
         if (row > n || col > n) {
             throw new IndexOutOfBoundsException("Out of index.");
         }
-        return isOpen(row, col) && percoUF.connected(xyTo1D(row, col), n * n)
-                && percoUF.connected(xyTo1D(row, col), n * n + 1);
-
+        return percoUF.connected(xyTo1D(row, col), n * n);
     }
 
     // change the xy correspond to 1D number
@@ -162,6 +116,4 @@ public class Percolation {
         System.out.println(x.numberOfOpenSites());
 
     }
-
-
 }
